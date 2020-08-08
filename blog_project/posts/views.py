ebc078f8d4192f416ff7b6b2e44bc2c4a4f6d736
Shajibur Rahman <shojibur@gmail.com>
2020-08-09 01:32:10 +0800

    Added custom permissions

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+
 from .models import Post
 from  .serializers import PostSerializer
 # Create your views here.
-
+from .permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
+    # Comma is important at the end, 
+    # This is app level permission, commenting since we set project level permission
+    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
